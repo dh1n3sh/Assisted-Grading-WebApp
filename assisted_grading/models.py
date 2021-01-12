08c0e8a4b623b46f9868a852819f91b7e7fd0c2e
Hououin <Okarin123>
2021-01-13 00:21:47 +0530

    update models- Yet to consider primary keys

--- assisted_grading/models.py
+++ assisted_grading/models.py
@@ -2,23 +2,60 @@ from django.db import models
 
 # Create your models here.
 
-
 class Professor(models.Model):
-    Name = models.CharField(max_length=50)
-    Email = models.CharField(max_length=50)
-    Department = models.CharField(max_length=20)
 
+    prof_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    department = models.CharField(max_length=20)
 
-# class Course(models.Model):
+class Course(models.Model):
 
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    
+    name = models.CharField (max_length = 50)
+    offering_dept = models.CharField (max_length = 50) 
+    course_id = models.CharField (max_length = 50) 
 
-# class Submission(models.Model):
+class Test(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    
+    date = models.DateField() 
+    duration = models.DurationField() 
+    consolidated_marksheet = models.FileField(upload_to='uploads/testMarksheets/')
 
+#https://docs.djangoproject.com/en/3.1/ref/models/fields/#filefield 
+class Submission(models.Model):
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
 
-# class Test(models.Model):
+    answerscript_pdf = models.FileField(upload_to='uploads/submissions/')
+    submission_time = models.DateTimeField()  
+    status = models.IntegerField() 
 
+class QpNode (models.Model): 
 
-# class QpTree(models.Model):
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    
+    question_number = models.CharField (max_length = 50) 
+    level = models.IntegerField() 
+    maxMarks = models.IntegerField() 
+    children_count = models.IntegerField() 
 
+class QpNodeAdjList(models.Model):
+     
+    parentNode = models.ForeignKey (QpNode, on_delete = models.CASCADE) 
+    childNode = models.ForeignKey (QpNode, on_delete = models.CASCADE) 
 
-# class GradeTree(QpTree):
+class GradeNode (QpNode):
+
+    submission = models.ForeignKey (Submission, on_delete = models.CASCADE)
+    awarded_marks = models.IntegerField() 
+    remarks = models.CharField(max_length = 100) 
+    status = models.IntegerField() 
+
+    segmentation_image = models.FileField(upload_to='uploads/segmentation_images/')
+    processed_image =  models.FileField(upload_to='uploads/processed_images/') 
+
+class GradeNodeAdjList (models.Model): 
+
+    parentNode = models.ForeignKey (GradeNode, on_delete = models.CASCADE) 
+    childNode = models.ForeignKey (GradeNode, on_delete = models.CASCADE) 
