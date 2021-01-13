d1f7734403bfe066f3947045ff7a8d5c6f6ab9d4
dh1n3sh <dhinesh17035@cse.ssn.edu.in>
2021-01-13 13:52:17 +0530

    removed adj list in models

--- assisted_grading/models.py
+++ assisted_grading/models.py
@@ -4,69 +4,77 @@ from django.db import models
 
 
 class Professor(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, null=True)
     email = models.EmailField(max_length=50, null=True)
-    department = models.CharField(max_length=20)
+    department = models.CharField(max_length=20, null=True)
 
 
 class Course(models.Model):
-    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    professor = models.ForeignKey(
+        Professor, on_delete=models.CASCADE, null=True)
 
     name = models.CharField(max_length=50)
-    offering_dept = models.CharField(max_length=50)
-    course_id = models.CharField(max_length=50)
+    offering_dept = models.CharField(max_length=50, null=True)
+    course_id = models.CharField(max_length=50, null=True)
 
 
 class Test(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
 
-    date = models.DateField()
-    duration = models.DurationField()
+    date = models.DateField(null=True)
+
     consolidated_marksheet = models.FileField(
-        upload_to='uploads/testMarksheets/')
+        upload_to='uploads/testMarksheets/',
+        null=True)
 
 # https://docs.djangoproject.com/en/3.1/ref/models/fields/#filefield
 
 
 class Submission(models.Model):
-    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, null=True)
 
-    answerscript_pdf = models.FileField(upload_to='uploads/submissions/')
-    submission_time = models.DateTimeField()
-    status = models.IntegerField()
+    answerscript_pdf = models.FileField(
+        upload_to='uploads/submissions/', null=True)
+    submission_time = models.DateTimeField(null=True)
+    status = models.IntegerField(null=True)
 
 
 class QpNode (models.Model):
 
-    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, null=True)
 
-    question_number = models.CharField(max_length=50)
-    level = models.IntegerField()
-    maxMarks = models.IntegerField()
-    children_count = models.IntegerField()
-
-
-class QpNodeAdjList(models.Model):
-
-    parentNode = models.ForeignKey(QpNode, on_delete=models.CASCADE)
-    childNode = models.ForeignKey(
-        QpNode, on_delete=models.CASCADE, related_name='QpdNodeToChild')
+    question_number = models.CharField(max_length=50, null=True)
+    max_marks = models.IntegerField(null=True)
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True
+    )
 
 
-class GradeNode (QpNode):
+class GradeNode (models.Model):
 
-    submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
-    awarded_marks = models.IntegerField()
-    remarks = models.CharField(max_length=100)
-    status = models.IntegerField()
+    submission = models.ForeignKey(
+        Submission, on_delete=models.CASCADE, null=True
+    )
+    question = models.ForeignKey(
+        QpNode, on_delete=models.CASCADE, null=True
+    )
+
+    awarded_marks = models.IntegerField(null=True)
+    remarks = models.CharField(max_length=100, null=True)
+    status = models.IntegerField(null=True)
 
     segmentation_image = models.FileField(
-        upload_to='uploads/segmentation_images/')
-    processed_image = models.FileField(upload_to='uploads/processed_images/')
-
-
-class GradeNodeAdjList (models.Model):
-
-    parentNode = models.ForeignKey(GradeNode, on_delete=models.CASCADE)
-    childNode = models.ForeignKey(
-        GradeNode, on_delete=models.CASCADE, related_name='GradeNodeToChild')
+        upload_to='uploads/segmentation_images/',
+        null=True
+    )
+    processed_image = models.FileField(
+        upload_to='uploads/processed_images/',
+        null=True
+    )
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True
+    )
