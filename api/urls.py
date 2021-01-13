7f9189a34188109f6972a14c020dfcb7700a4154
dh1n3sh <dhinesh17035@cse.ssn.edu.in>
2021-01-13 21:35:55 +0530

    login

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('', views.index, name='index'),
-    path ('courses/', views.getCourses, name='getCourses') 
+    path('', views.index, name='index'),
+    path('login/', views.userLogin, name='PostLogin'),
+    path('courses/', views.getCourses, name='getCourses')
 ]
--- api/views.py
+++ api/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Course 
+from .models import Course, Professor
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the api index.")
 
-def getCourses (request): 
-    
-    print (request.user) 
-    prof = request.GET['user']  
-    print (prof) 
 
-    return HttpResponse ("dummy response") 
+def userLogin(request):
+    user = authenticate(
+        username=request.GET['username'], password=request.GET['password'])
+    if user is not None:
+        login(request, user)
+        return HttpResponse("Hey "+request.GET['username'])
+    else:
+        return HttpResponse("Failed")
+
+
+def getCourses(request):
+
+    # print(request.user)
+    # prof = request.GET['user']
+    # print(prof)
+
+    return HttpResponse(request.user.professor.name)
