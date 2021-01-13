7a9e4e8cee9015ffca837e77372fe34e694d357b
Hououin <Okarin123>
2021-01-13 20:53:10 +0530

    update api for prelimenary tests

--- api/views.py
+++ api/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Course 
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the api index.")
+
+def getCourses (request): 
+    
+    print (request.user) 
+    prof = request.GET['user']  
+    print (prof) 
+
+    return HttpResponse ("dummy response") 
