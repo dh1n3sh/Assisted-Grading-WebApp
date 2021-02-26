b8e7dc622911e71b1da2a0dc3dc52cebe6dadfa6
Amogh Gupta <amogh17009@cse.ssn.edu.in>
2021-02-26 16:52:30 +0530

    update handwriting code. Need to update preprocessing and requirements

--- api/jobs/handwriting.py
+++ api/jobs/handwriting.py
@@ -1,5 +1,10 @@
 from api.models import Student
 import logging
+
+import tensorflow as tf 
+import numpy as np 
+from PIL import Image
+
 logger = logging.getLogger()
 def verify_handwriting(student_class, answerscript_pdf):
     '''
@@ -12,10 +17,35 @@ def verify_handwriting(student_class, answerscript_pdf):
     model_path = ''
     if len(s)>0:
         model_path = s[0].handwriting_model
+    
     # loadmodel
+    model = tf.keras.models.load_model(model_path) 
 
-    # open pdf and pre process
+    # open pdf and pre process into 500*1000*1 strips
+    paths = list()
+    # Fill paths aptly  
+    test_images = np.zeros ((len(paths), 500, 1000, 1)) 
+    
+    
+    for i in range (len(paths)): 
 
-    # predict
+        path = paths[i] 
+        img = Image.open (path) 
+        data = np.array(img) 
+        data = data.reshape ((data.shape[0], data.shape[1], 1)) 
+
+        test_images[i] = data  
+    
+    test_images = test_images/255.0 
+    y_pred = model.predict_classes (test_images)  
+  
     logger.info('model '+str(model_path)+student_class)
-    return True
+    score = sum(y_pred)/len(y_pred)
+
+    #Set threshold 
+    threshold = 0.5 
+
+    if (score >= threshold): 
+        return True 
+    else:
+        return False
