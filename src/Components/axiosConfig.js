ebff193b95b036b7286cbe59124dc6f1c0c99b2e
dh1n3sh <dhinesh17035@cse.ssn.edu.in>
2021-03-30 23:31:53 +0530

    axios

--- src/Components/axiosConfig.js
+++ src/Components/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 axios.defaults.xsrfHeaderName = "X-CSRFToken"
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.withCredentials = true
-axios.defaults.headers.get['Access-Control-Allow-Origin'] ='true';
-axios.defaults.baseURL = "https://assisted-grading.herokuapp.com/"
+// axios.defaults.headers.get['Access-Control-Allow-Origin'] ='true';
+// axios.defaults.baseURL = "https://assisted-grading.herokuapp.com/"
 
 export default axios
